3b212e03a85e08cf2e44bd9290fe6fb7e045cafd
Josy Hartig <josy_hartig@yahoo.de>
2019-11-06 18:14:35 +0800

    improve mobile responsiveness

--- src/components/URLList/URLList.js
+++ src/components/URLList/URLList.js
@@ -3,11 +3,24 @@ import classes from './URLList.module.scss';
 import ListItem from './ListItem/ListItem';
 
 const URLList = (props) => {
-  const urlList = props.urls.map(url => <ListItem
-    key={url.url}
-    url={url.url}
-    shortUrl={url.shortURL}
-    copy={props.copy}/>)
+  const urlList = props.urls.map(url => {
+    let URL = url.url
+      if(window.innerWidth < '500' && url.url.length > 30) {
+        URL = url.url.substring(0, 30).concat('...');
+      } else if(window.innerWidth < '700' && window.innerWidth > '500' && url.url.length > 50) {
+        URL = url.url.substring(0, 50).concat('...');
+      } else if(window.innerWidth < '900' && window.innerWidth > '700' && url.url.length > 70) {
+        URL = url.url.substring(0, 70).concat('...');
+      }
+  
+  return (
+    <ListItem
+        key={url.url}
+        url={URL}
+        shortUrl={url.shortURL}
+        copy={props.copy}/>
+  )
+  });
   return (
     <div className={classes.URLList}>
         {urlList}
