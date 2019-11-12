0058b13d0683621bc49706f96146d09437777436
Josy Hartig <josy_hartig@yahoo.de>
2019-11-12 17:14:41 +0800

    improve responsiveness input, features

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -15,18 +15,15 @@ const Features = () => {
     <div className={classes.details}>
         <Feature
           heading="Brand Recognition"
-          paragraph="Boost your brand recognition with each click. Generic links don’t
-          mean a thing. Branded links help instil confidence in your content."
+          paragraph="Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content."
           img={recognition}/>
         <Feature
           heading="Detailed Records"
-          paragraph="Gain insights into who is clicking your links. Knowing when and where
-          people engage with your content helps inform better decisions."
+          paragraph="Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions."
           img={records}/>
         <Feature
           heading="Fully Customizable"
-          paragraph="Improve brand awareness and content discoverability through customizable
-          links, supercharging audience engagement."
+          paragraph="Improve brand awareness and content discoverability through customizable links, supercharging audience engagement."
           img={customizable}/>
       </div>
     </div>
